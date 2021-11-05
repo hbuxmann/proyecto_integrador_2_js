2393771dac8309b0a3c537d68829977a63781e1a
hbuxmann <hbuxmann@gmail.com>
2021-11-04 21:44:16 -0300

    composer!

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9a42e0730c7a29ac853f2594bd6add1554cf94d5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9a42e0730c7a29ac853f2594bd6add1554cf94d5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
